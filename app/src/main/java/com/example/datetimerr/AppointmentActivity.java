ec3dda7f80c76642f4e1bd3bac42d95ebdafb123
Park Sihyun <32purp1nk@gmail.com>
2023-08-16 16:05:28 +0900

    add_addbtn

--- app/src/main/java/com/example/datetimerr/AppointmentActivity.java
+++ app/src/main/java/com/example/datetimerr/AppointmentActivity.java
@@ -9,35 +9,55 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class AppointmentActivity extends AppCompatActivity {
 
     private DatePicker datePicker;
     private TimePicker timePicker;
+    private Button addDateTimeButton;
+    private Button createAppointmentButton;
+    private FirebaseAuth mAuth;
+
+    private List<Date> selectedDateTimes = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_appointment);
 
+        mAuth = FirebaseAuth.getInstance();
+
         datePicker = findViewById(R.id.datePicker);
         timePicker = findViewById(R.id.timePicker);
+        addDateTimeButton = findViewById(R.id.buttonAddDateTime);
+        createAppointmentButton = findViewById(R.id.buttonCreateAppointment);
 
-        Button createAppointmentButton = findViewById(R.id.buttonCreateAppointment);
+        addDateTimeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addSelectedDateTime();
+            }
+        });
+
         createAppointmentButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                createAppointment();
+                createAppointments();
             }
         });
     }
 
-    private void createAppointment() {
+    private void addSelectedDateTime() {
         int year = datePicker.getYear();
         int month = datePicker.getMonth();
         int dayOfMonth = datePicker.getDayOfMonth();
@@ -48,25 +68,41 @@ public class AppointmentActivity extends AppCompatActivity {
         Calendar calendar = Calendar.getInstance();
         calendar.set(year, month, dayOfMonth, hour, minute);
 
-        String creator = "John"; // 약속 생성자 이름
+        selectedDateTimes.add(calendar.getTime());
+    }
 
-        Map<String, Object> appointmentData = new HashMap<>();
-        appointmentData.put("creator", creator);
-        appointmentData.put("dateTime", calendar.getTime());
+    private void createAppointments() {
+        String creator = getCurrentUserDisplayName(); // 로그인한 사용자 이름 가져오기
 
         FirebaseFirestore db = FirebaseFirestore.getInstance();
-        db.collection("appointments")
-                .add(appointmentData)
-                .addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
-                    @Override
-                    public void onComplete(@NonNull Task<DocumentReference> task) {
-                        if (task.isSuccessful()) {
-                            // 약속 정보 저장 성공
-                        } else {
-                            // 약속 정보 저장 실패
+
+        for (Date dateTime : selectedDateTimes) {
+            Map<String, Object> appointmentData = new HashMap<>();
+            appointmentData.put("creator", creator);
+            appointmentData.put("dateTime", dateTime);
+
+            db.collection("appointments")
+                    .add(appointmentData)
+                    .addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
+                        @Override
+                        public void onComplete(@NonNull Task<DocumentReference> task) {
+                            if (task.isSuccessful()) {
+                                // 약속 정보 저장 성공
+                            } else {
+                                // 약속 정보 저장 실패
+                            }
                         }
-                    }
-                });
+                    });
+        }
+    }
+
+    // 현재 로그인한 사용자의 표시 이름 가져오기
+    private String getCurrentUserDisplayName() {
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null) {
+            return currentUser.getDisplayName();
+        }
+        return "Unknown";
     }
 
     // 다른 메소드들...
